6535905cf5dd0cd636eefa6a6e48b50366b17393
cfoo98 <33429961+cfoo98@users.noreply.github.com>
2020-11-19 21:06:12 +0000

    Add files via upload

---
+++ stamen.neatline.js
@@ -0,0 +1,129 @@
+(function(exports) {
+
+/*
+ * tile.stamen.js v1.2.1
+ */
+
+var SUBDOMAINS = " a. b. c. d.".split(" "),
+    MAKE_PROVIDER = function(layer, type, minZoom, maxZoom) {
+        return {
+            "url":          ["http://{S}tile.stamen.com/", layer, "/{Z}/{X}/{Y}.", type].join(""),
+            "type":         type,
+            "subdomains":   SUBDOMAINS.slice(),
+            "minZoom":      minZoom,
+            "maxZoom":      maxZoom,
+            "attribution":  ATTRIBUTION
+        };
+    },
+    PROVIDERS =  {
+        "toner":        MAKE_PROVIDER("toner", "png", 0, 20),
+        "terrain":      MAKE_PROVIDER("terrain", "jpg", 4, 18),
+        "watercolor":   MAKE_PROVIDER("watercolor", "jpg", 1, 16),
+        "trees-cabs-crime": {
+            "url": "http://{S}.tiles.mapbox.com/v3/stamen.trees-cabs-crime/{Z}/{X}/{Y}.png",
+            "type": "png",
+            "subdomains": "a b c d".split(" "),
+            "minZoom": 11,
+            "maxZoom": 18,
+            "extent": [
+                {"lat": 37.853, "lon": -122.577},
+                {"lat": 37.684, "lon": -122.313}
+            ],
+            "attribution": [
+                'Design by Shawn Allen at <a href="http://stamen.com">Stamen</a>.',
+                'Data courtesy of <a href="http://fuf.net">FuF</a>,',
+                '<a href="http://www.yellowcabsf.com">Yellow Cab</a>',
+                '&amp; <a href="http://sf-police.org">SFPD</a>.'
+            ].join(" ")
+        }
+    },
+    ATTRIBUTION = [
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
+        'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
+        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+    ].join("");
+
+// set up toner and terrain flavors
+setupFlavors("toner", ["hybrid", "labels", "lines", "background", "lite"]);
+// toner 2010
+setupFlavors("toner", ["2010"]);
+// toner 2011 flavors
+setupFlavors("toner", ["2011", "2011-lines", "2011-labels", "2011-lite"]);
+setupFlavors("terrain", ["background"], "png");
+
+/*
+ * Export stamen.tile to the provided namespace.
+ */
+exports.stamen = exports.stamen || {};
+exports.stamen.tile = exports.stamen.tile || {};
+exports.stamen.tile.providers = PROVIDERS;
+exports.stamen.tile.getProvider = getProvider;
+
+/*
+ * A shortcut for specifying "flavors" of a style, which are assumed to have the
+ * same type and zoom range.
+ */
+function setupFlavors(base, flavors, type) {
+    var provider = getProvider(base);
+    for (var i = 0; i < flavors.length; i++) {
+        var flavor = [base, flavors[i]].join("-");
+        PROVIDERS[flavor] = MAKE_PROVIDER(flavor, type || provider.type, provider.minZoom, provider.maxZoom);
+    }
+}
+
+/*
+ * Get the named provider, or throw an exception if it doesn't exist.
+ */
+function getProvider(name) {
+    if (name in PROVIDERS) {
+        return PROVIDERS[name];
+    } else {
+        throw 'No such provider (' + name + ')';
+    }
+}
+
+/*
+ * StamenTileLayer for OpenLayers
+ * <http://openlayers.org/>
+ *
+ * Tested with v2.1x.
+ */
+if (typeof OpenLayers === "object") {
+    // make a tile URL template OpenLayers-compatible
+    function openlayerize(url) {
+        return url.replace(/({.})/g, function(v) {
+            return "$" + v.toLowerCase();
+        });
+    }
+
+    // based on http://www.bostongis.com/PrinterFriendly.aspx?content_name=using_custom_osm_tiles
+    OpenLayers.Layer.Stamen = OpenLayers.Class(OpenLayers.Layer.OSM, {
+        initialize: function(name, options) {
+            var provider = getProvider(name),
+                url = provider.url,
+                subdomains = provider.subdomains,
+                hosts = [];
+            if (url.indexOf("{S}") > -1) {
+                for (var i = 0; i < subdomains.length; i++) {
+                    hosts.push(openlayerize(url.replace("{S}", subdomains[i])));
+                }
+            } else {
+                hosts.push(openlayerize(url));
+            }
+            options = OpenLayers.Util.extend({
+                "numZoomLevels":        provider.maxZoom,
+                "buffer":               0,
+                "transitionEffect":     "resize",
+                // see: <http://dev.openlayers.org/apidocs/files/OpenLayers/Layer/OSM-js.html#OpenLayers.Layer.OSM.tileOptions>
+                // and: <http://dev.openlayers.org/apidocs/files/OpenLayers/Tile/Image-js.html#OpenLayers.Tile.Image.crossOriginKeyword>
+                "tileOptions": {
+                    "crossOriginKeyword": null
+                }
+            }, options);
+            return OpenLayers.Layer.OSM.prototype.initialize.call(this, name, hosts, options);
+        }
+    });
+}
+
+})(typeof exports === "undefined" ? this : exports);
